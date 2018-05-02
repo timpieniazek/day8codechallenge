bc4590103f7a01d5acca7e4d3c3c086c2df45f72
Tim Pieniazek <timpieniazek@gmail.com>
2018-05-02 12:06:32 -0400

    JavaApril18 Day 8 Code Challenge

---
+++ src/ArrayListCodeChallenge.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+/*
+ * Write a method that accepts an ArrayList of Strings and and int for an index
+ * Modify the name in the ArrayList at the index specified to "Merc"
+ */
+public class ArrayListCodeChallenge {
+
+	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<>();
+		names.add("Vicky");
+		names.add("Brent");
+		names.add("Julhasur");
+		names.add("Alli");
+		names.add("Mike");
+		names.add("Anthony");
+		names.add("John");
+		names.add("Tim");
+		names.add("Jonah");
+		names.add("Jiao");
+		names.add("Ben");
+		
+		mercReplacer(names, 8);
+		
+		System.out.println(names);
+
+	}
+	
+	// your method should go here
+	public static ArrayList<String> mercReplacer(ArrayList<String> list, int index) {
+		list.set(index, "Merc");
+		return list;
+	}
+
+
+}
